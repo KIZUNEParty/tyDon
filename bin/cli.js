cdc9dab1e7fc9317107999a5bdc2987c20557d36
dethMastery <me@suphakit.net>
2023-03-11 09:56:16 +0700

    feat: update package json

---
+++ bin/cli.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+const yargs = require('yargs');
+
+const opt = yargs
+    .usage(
+        "Usage: 'tyDon' or 'tyDon -d <Your Custom Date>'"
+    )
+    .option("date", {
+        alias: 'd',
+        describe: "Custom Date",
+        type: "string",
+        demandOption: false
+    })
+    .argv;
+
+const a = `${opt.d}`;
+
+if( a != 'undefined') {
+    const moonLanding = new Date(a);
+    console.log(moonLanding.getTime());
+} else {
+    const moonLanding = new Date;
+    console.log(moonLanding.getTime());
+}
