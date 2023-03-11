d3d11cd157f3129d0c4cece6bfecbf2dee842add
dethMastery <me@suphakit.net>
2023-03-11 10:03:21 +0700

    feat: add module based

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,9 @@
-#! /usr/bin/env node
-
-const yargs = require('yargs');
-
-const opt = yargs
-    .usage(
-        "Usage: 'tyDon' or 'tyDon -d <Your Custom Date>'"
-    )
-    .option("date", {
-        alias: 'd',
-        describe: "Custom Date",
-        type: "string",
-        demandOption: false
-    })
-    .argv;
-
-const a = `${opt.d}`;
-
-if( a != 'undefined') {
+exports.tyDon = function(date) {
+  if (date != undefined) {
     const moonLanding = new Date(a);
     console.log(moonLanding.getTime());
-} else {
-    const moonLanding = new Date;
+  } else {
+    const moonLanding = new Date();
     console.log(moonLanding.getTime());
-}
+  }
+  }
